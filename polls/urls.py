97d76188c5f1c70b45a30c75368dc7ccda605c9c
Ajie Pangestu <paangestu@gmail.com>
2021-09-14 16:46:45 +0700

    add detail controller

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns = [
 	path('', view=views.index, name='index'),
 	path('<int:question_id>/', view=views.detail, name='detail'),
