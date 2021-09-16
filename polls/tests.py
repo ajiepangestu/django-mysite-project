6264449b899a5f109adab99a9d83cc8879ad6c1c
Ajie Pangestu <paangestu@gmail.com>
2021-09-16 22:34:07 +0700

    add detail view test

--- polls/tests.py
+++ polls/tests.py
@@ -83,3 +83,25 @@ class QuestionIndexViewTest(TestCase):
             response.context['latest_question_list'],
             [question2, question1]
         )
+
+
+class QuestionDetailViewTest(TestCase):
+    def test_future_question(self):
+        future_question = create_question(
+            question_text='Future Question',
+            days=5
+        )
+        response = self.client.get(
+            reverse('polls:detail', args=(future_question.id,))
+        )
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(
+            question_text='Past Question',
+            days=-5
+        )
+        response = self.client.get(
+            reverse('polls:detail', args=(past_question.id,))
+        )
+        self.assertContains(response, past_question.question_text)
